eb2af7a5f53e350cd2d7ebb4e991bf52924aaecd
PeterHiroshi <peter8icestone@gmail.com>
2020-07-29 02:45:41 +0900

    IPersistence analysis config test

--- src/test/java/com/peter8icestone/test/IPersistenceTest.java
+++ src/test/java/com/peter8icestone/test/IPersistenceTest.java
@@ -1,15 +1,28 @@
 package com.peter8icestone.test;
 
+import com.peter8icestone.config.XMLConfigBuilder;
 import com.peter8icestone.io.Resources;
+import com.peter8icestone.pojo.Configuration;
+import org.dom4j.DocumentException;
 
+import java.beans.PropertyVetoException;
 import java.io.InputStream;
 import java.util.Optional;
 
 public class IPersistenceTest {
 
     private static void test() {
-        InputStream resourceAsStream = Resources.getResourceAsStream("userMapper.xml");
-        Optional.ofNullable(resourceAsStream).ifPresent(System.out::println);
+        InputStream sqlMapConfigStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder();
+        Configuration configuration = null;
+        try {
+            configuration = xmlConfigBuilder.parseConfig(sqlMapConfigStream);
+        } catch (DocumentException | PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        Optional.ofNullable(configuration)
+                .map(Configuration::getMappedStatementMap)
+                .ifPresent(System.out::println);
     }
 
     public static void main(String[] args) {
