79d497d1c497b1046afde2d3e7a79252d3869a4a
PeterHiroshi <peter8icestone@gmail.com>
2020-08-01 18:37:55 +0900

    add test fields

--- src/test/java/com/peter8icestone/test/IPersistenceTest.java
+++ src/test/java/com/peter8icestone/test/IPersistenceTest.java
@@ -14,12 +14,13 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class IPersistenceTest {
 
     @Test
-    public void test() {
+    public void testSelectOne() {
         InputStream sqlMapConfigStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = null;
         try {
@@ -36,6 +37,14 @@ public class IPersistenceTest {
                 .map(sqlSession -> sqlSession.selectOne(statementId, user))
                 .orElse(null);
         Optional.ofNullable(selectedUser).ifPresent(System.out::println);
+    }
+
+    @Test
+    public void testSelectList() throws PropertyVetoException, DocumentException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        List<User> users = sqlSessionFactory.openSession().selectList("user.selectList");
+        users.forEach(System.out::println);
     }
 
     @Test
